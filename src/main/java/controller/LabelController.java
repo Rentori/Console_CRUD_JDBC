6d98c7f391153c9e76be7d061f38a424034210fe
rentorry <vitya.erofeev.90@gmail.com>
2021-06-16 11:32:24 +0200

    removed implementation

--- src/main/java/controller/LabelController.java
+++ src/main/java/controller/LabelController.java
@@ -1,35 +1,29 @@
 package controller;
 
 import model.Label;
-import repository.GenericRepository;
 import service.LabelService;
 
 import java.util.List;
 
-public class LabelController implements GenericRepository<Label, Long> {
+public class LabelController {
     LabelService labelService = new LabelService();
 
-    @Override
     public Label save(Label type) {
         return labelService.save(type);
     }
 
-    @Override
     public Label update(Label type) {
         return labelService.update(type);
     }
 
-    @Override
     public Label getById(Long aLong) {
         return labelService.getById(aLong);
     }
 
-    @Override
     public void deleteById(Long aLong) {
         labelService.deleteById(aLong);
     }
 
-    @Override
     public List<Label> getAll() {
         return labelService.getAll();
     }
